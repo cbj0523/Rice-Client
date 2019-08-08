d318b8b750b156bd6771fe8793df355bf45a2103
박태진 <46419512+Dev-Taejin@users.noreply.github.com>
2019-08-08 16:48:54 +0900

    학교목록 디자인 수정

--- get_meal.js
+++ get_meal.js
@@ -4,7 +4,8 @@ var charset = require('charset');
 
 
 module.exports.getSchoolMeal = function (office, schoolcode, year, month, date) {
-    var url = `http://localhost:10/?office_of_education=${office}&school_code=${schoolcode}&year=${year}&month=${month}&date=${date}`;
+    var token = "N7hF1WPya01sJFLBvPVZWPFoo0d7lqqeKaLqU2DnhOuiDSa15qS9K5XeAhzwlIPXJk2Y1TjeK5iWvVIf8VDMuGWu4vA8RL0TnChnkqFpFHa6JmzB2VFVI3iZddXme4Ll";
+    var url = `http://localhost:10/?token=${token}&office_of_education=${office}&school_code=${schoolcode}&year=${year}&month=${month}&date=${date}`;
     request(url, (err, res, body)=> {
         console.log(url);
         console.log(body);
